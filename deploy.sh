24f3f2aa7244aa08f6d2421e72f2b5719a83fc11
Juan Carlos Cuadrado Gracia <jccuadradogracia@gmail.com>
2023-02-02 18:12:05 +0100

    caca

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:JCCG-code/t-rivial.git master:gh-pages
+git push -f git@github.com:t-children/t-rivial.git master:gh-pages
 
 cd -
